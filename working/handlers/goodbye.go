bc80d1325c216f4feb9885574b4767531fcc6fa1
EricRootLee <erickiokom@gmail.com>
2021-03-26 00:26:04 +0300

    Clean

--- working/handlers/goodbye.go
+++ working/handlers/goodbye.go
@@ -13,17 +13,11 @@ type Goodbye struct {
 
 func (g *Goodbye) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	g.l.Println("Root Route") //logg
-	d,_:=ioutil.ReadAll(request.Body)
-	fmt.Fprintf(writer,"Data %s",d)
+	d, _ := ioutil.ReadAll(request.Body)
+	fmt.Fprintf(writer, "Data %s", d)
 	writer.Write([]byte("Hello Goodbye"))
 }
 
-func (g *Goodbye) ServerHTTP(writer http.ResponseWriter,request *http.Request) {
-
-}
-
-
-func NewGoodbye(l*log.Logger) *Goodbye  {
+func NewGoodbye(l *log.Logger) *Goodbye {
 	return &Goodbye{l}
 }
-
