7b926a60574cd1dd694c189fd7c2ca2f269f77d7
EricRootLee <erickiokom@gmail.com>
2021-03-25 17:34:09 +0300

    curl

--- working/main.go
+++ working/main.go
@@ -9,6 +9,10 @@ func main()  {
 	http.HandleFunc("/",func(http.ResponseWriter , *http.Request){
 		log.Println("Hello world")
 	})
+	
+	http.HandleFunc("/goodbye", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Goodbye")
+	})
 
 	http.ListenAndServe(":9090",nil)
 }
