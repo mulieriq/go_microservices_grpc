2a8643ddf99d7416a1aa57d29c6f25f554d3ad49
EricRootLee <erickiokom@gmail.com>
2021-03-25 17:25:30 +0300

    handle func

--- working/main.go
+++ working/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main()  {
+	http.HandleFunc("/",func(http.ResponseWriter , *http.Request){
+		log.Println("Hello world")
+	})
+
 	http.ListenAndServe(":9090",nil)
 }
