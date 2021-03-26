6bf7accaf8618a7d3153e517a79f6e4da2fb8edc
EricRootLee <erickiokom@gmail.com>
2021-03-26 18:08:19 +0300

    Refactored

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -3,30 +3,30 @@ package data
 import "time"
 
 type Product struct {
-	ID int
-	Name string
+	ID          int
+	Name        string
 	Description string
-	Price float32
-	SKU string
-	CreatedOn string
-	UpdatedOn string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
 }
 
 var productList = []*Product{
-	&Product{
-		ID: 1,
-		Name: "Latte",
+	{
+		ID:          1,
+		Name:        "Latte",
 		Description: "Milky Coffee",
-		Price: 2.45,
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
-	},
-	&Product{
-		ID:2,
-		Name: "Espresso",
+		Price:       2.45,
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+		},
+	{
+		ID:          2,
+		Name:        "Espresso",
 		Description: "String Coffee",
-		Price: 1.99,
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
-	},
+		Price:       1.99,
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+		},
 }
