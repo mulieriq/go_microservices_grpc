53092670471ff71d76ea829c8cb99f418681afd6
EricRootLee <erickiokom@gmail.com>
2021-03-28 00:07:38 +0300

    Server http handler

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 type Products struct {
 	l *log.Logger
@@ -8,4 +11,8 @@ type Products struct {
 
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
+}
+
+func (p *Products) ServerHTTP(w http.ResponseWriter, response *http.Response) {
+
 }
