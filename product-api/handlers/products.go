ece3a70b13670182b5b4a7f27e36eadef7721fed
EricRootLee <erickiokom@gmail.com>
2021-03-28 00:24:25 +0300

    returning data

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"product-api/product-api/data"
 )
 
 type Products struct {
@@ -14,5 +16,10 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(w http.ResponseWriter, response *http.Request) {
-
+	lp := data.GetProducts()
+	data, err := json.Marshal(lp)
+	if err != nil {
+		http.Error(w, "Unable to parse data", http.StatusInternalServerError)
+	}
+	w.Write(data)
 }
